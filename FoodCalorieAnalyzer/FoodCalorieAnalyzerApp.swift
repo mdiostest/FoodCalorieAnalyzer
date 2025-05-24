7a231d5a218275db51898da661949dd3e0bfc195
mdiostest <mdiostest@example.com>
2025-05-24 09:22:36 +0530

    feat: implement app entry point

---
+++ FoodCalorieAnalyzer/FoodCalorieAnalyzerApp.swift
@@ -0,0 +1,13 @@
+import SwiftUI
+
+@main
+struct FoodCalorieAnalyzerApp: App {
+    let persistenceController = CoreDataManager.shared
+    
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+                .environment(\.managedObjectContext, persistenceController.context)
+        }
+    }
+} 
